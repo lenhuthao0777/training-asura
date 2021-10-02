15b9bf0427e39e24e34f477a9b7806c43d00ce0e
Le Nhut Hao <lenhuthao0777@gmail.com>
2021-10-02 19:47:34 +0700

    feat(formTask): fix change input

--- src/pages/Home/FormTask/container/FormTaskContainer.js
+++ src/pages/Home/FormTask/container/FormTaskContainer.js
@@ -3,24 +3,12 @@ import { useSelector } from "react-redux";
 import FormTask from "../components/FormTask";
 function FormContainer() {
     const { taskDetail } = useSelector((state) => state.TaskReducer);
-    const [taskDetailData, setTaskDetailData] = useState({
-        id: taskDetail.id,
-        taskName: taskDetail.taskName,
-        time: taskDetail.time,
-        name: taskDetail.name,
-        dateOfBirth: taskDetail.dateOfBirth,
-        address: taskDetail.address,
-        phone: taskDetail.phone,
-        email: taskDetail.email,
-        currentJob: taskDetail.currentJob,
-        experience: taskDetail.experience,
-        note: taskDetail.note,
-    });
+    const [taskDetailData, setTaskDetailData] = useState({});
 
     useEffect(() => {
         setTaskDetailData(taskDetail);
     }, [taskDetail]);
-    return <FormTask taskDetailData={taskDetailData} setTaskDetailData={setTaskDetailData}/>;
+    return <FormTask taskDetailData={taskDetailData} setTaskDetailData={setTaskDetailData} />;
 }
 
 export default FormContainer;
